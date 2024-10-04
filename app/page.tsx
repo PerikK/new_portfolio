b4ee99f11e781282cbdb997b82788f113139a866
PerikK <perik76@gmail.com>
2024-10-04 18:44:44 +0300

    add shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 export default function Home() {
   return (
-    <section className="py-16 px-8">
-      <div className="container">
-        <h1 className="font-bold text-3xl">Portfolio</h1>
+    <section className='px-8 py-16'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Portfolio</h1>
       </div>
     </section>
-  );
+  )
 }
